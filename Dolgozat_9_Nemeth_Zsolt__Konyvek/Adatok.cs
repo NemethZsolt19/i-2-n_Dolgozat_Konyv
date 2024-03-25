9002bf8a7de181978b77c11cbb255b2a02221308
NemethZsolt19 <153748827+NemethZsolt19@users.noreply.github.com>
2024-03-25 14:47:44 +0100

    Add files via upload

---
+++ Dolgozat_9_Nemeth_Zsolt__Konyvek/Adatok.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konyvek
+{
+    public class Adatok
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public int Pages;
+
+        public Adatok(string title, string author, int year, int pages)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        public override string? ToString()
+        {
+            return Author+": "+Title+" (Kiadási éve: "+Year+", "+Pages+" oldal.)";
+        }
+    }
+}
---
+++ Dolgozat_9_Nemeth_Zsolt__Konyvek/Program.cs
@@ -0,0 +1,7 @@
+﻿using Konyvek;
+using System.Runtime.CompilerServices;
+
+Adatok konyv = new Adatok("Arni és az Alma", "Nagy Lajos",2020, 50);
+Adatok konyv2 = new Adatok("Kossuth Lajos Élete", "Vas Zoltán", 1983, 1768);
+Console.WriteLine(konyv);
+Console.WriteLine(konyv2);
